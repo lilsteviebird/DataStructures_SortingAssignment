f8ec7adeb967d8c04f7702a0d4a21d8f510b96cf
lilsteviebird <stevenli880@gmail.com>
2019-01-13 21:24:56 -0500

    Initial Commit

---
+++ Assignment8Real/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment8Real
+//
+//  Created by sli19 on 1/13/19.
+//  Copyright © 2019 sli19. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
